6bf73f8cb76db996292ddd14e2800c887bad1718
Max M <maks5050@mail.ru>
2024-05-03 00:00:13 +0300

    add greetting func(greet.py) and ask name func(cli.py)

---
+++ brain_games/greet.py
@@ -0,0 +1,3 @@
+def greeting():
+    print('Welcome to the Brain Games!')
+
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
-from brain_games.scripts.greet import greeting
+from brain_games.greet import greeting
+from brain_games.cli import welcome_user
 
 def main():
     greeting()
+    welcome_user()
 
 if __name__ == '__main__':
     main()
