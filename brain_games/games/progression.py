66aefa8433c8fcb5eac8a0eb8725ee53e39893f7
Max M <maks5050@mail.ru>
2024-05-05 22:48:36 +0300

    fix ebanyu progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -10,31 +10,25 @@ def progression():
     print(Fore.YELLOW + 'What number is missing in the progression?')
     tries = 0
     while tries < 3:
-        list_of_numbers = []
-        random_len = random.randint(5, 10)
-        random_start_number = random.randint(1, 10)
-        rand_inter_number = random.randint(2, 10)
-        random_number_hide = random.randint(1, random_len)
+        list_of_numbers = list(range(1, 100))
+        l_f_k = []
+        random_interval_number = random.randint(2, 9)
+        interval_number = random_interval_number
         string_of_question = ""
-        while len(list_of_numbers) <= random_len:
-            if len(list_of_numbers) == 0:
-                list_of_numbers.append(random_start_number)
+        while len(l_f_k) < 10:
+            l_f_k.append(list_of_numbers[interval_number])
+            interval_number += random_interval_number
+        for i in l_f_k:
+            if len(string_of_question) > 1:
+                string_of_question += str(i) + ' '
             else:
-                list_of_numbers.append(list_of_numbers[-1] + rand_inter_number)
-        for i in list_of_numbers:
-            if list_of_numbers.index(i) == random_number_hide:
-                string_of_question += ".."
-                res = i
-                string_of_question += " "
-            else:
-                string_of_question += str(i) + " "
+                string_of_question += ".. "
         print('Question: ' + string_of_question)
-        answer = prompt.string('Your answer: ')
-        if answer == str(res):
-            print(Fore.GREEN + 'Correct!')
-            res = 1
+        a = prompt.string('Your answer: ')
+        if a == str(l_f_k[0]):
+            print('Correct!')
         else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{res}'.")
+            print(f"'{a}' is wrong answer ;(. Correct answer was '{l_f_k[0]}'.")
             print(f"Let's try again, {name}!")
             return
         tries += 1
